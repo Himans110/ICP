fe0597bb475fb0cb77445a471dde0af0ba41840f
Himans110 <himanshgoel110@gmail.com>
2025-10-01 21:51:55 +0700

    Update Find All Anagrams in a String.java

--- Assignment4/Find All Anagrams in a String.java
+++ Assignment4/Find All Anagrams in a String.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> l = new ArrayList<>();
-        for(int i = 0 ; i < s.length()-p.length()+1 ; i++){
-            String ans = s.substring(i,i+p.length());
-            if(anagran(ans,p)) l.add(i);
-        }
-        return l;
-    }
-    public boolean anagran(String s1 , String s2){
+        List<Integer> ans = new ArrayList<>();
+        if(p.length() > s.length()) return ans;
         int[] freq1 = new int[26];
-        int[] freq2 = new int[26];
-        for (int i = 0; i < s1.length(); i++) {
-            freq1[s1.charAt(i) - 'a']++;
+        int freq2[] = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            freq1[p.charAt(i) - 'a']++;
+            freq2[s.charAt(i)-'a']++;
         }
-        for (int i = 0; i < s2.length(); i++) {
-            freq2[s2.charAt(i) - 'a']++;
+        if(compare(freq1,freq2)) ans.add(0);
+        int k = p.length();
+        for(int i = k ; i < s.length() ; i++){
+            freq2[s.charAt(i-k)-'a']--;
+            freq2[s.charAt(i)-'a']++;
+            if(compare(freq1,freq2)) ans.add(i-k+1);
         }
+        return ans;
+    }
+    public boolean compare(int freq1[] , int freq2[]){
         for (int i = 0; i < 26; i++) {
             if (freq1[i] != freq2[i]) return false;
         }
